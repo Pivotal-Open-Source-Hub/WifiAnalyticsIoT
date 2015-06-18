cf85d19c0fc874aafaa885b3e4897437f901a85c
melofred <frederico.melo@gmail.com>
2015-06-18 23:29:58 +0000

    clean-up

--- client/startMonitor.sh
+++ client/startMonitor.sh
@@ -1,2 +1,1 @@
 sudo airmon-ng start wlan1 6
-#sudo tshark -i wlan1mon -I -f 'broadcast' -R 'wlan.fc.type == 0 && wlan.fc.subtype == 4' -T fields -e frame.time_epoch -e wlan.sa -e radiotap.dbm_antsignal
