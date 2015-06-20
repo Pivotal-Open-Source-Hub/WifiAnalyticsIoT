c56180c309b0e481b5700b720ce62f8afa3dc80e
fmelo <fmelo@fredericos-air>
2015-06-20 09:10:06 -0700

    bug fix

--- client/IoT-Analytics/src/main/java/io/pivotal/demo/ProbeCaptureRunner.java
+++ client/IoT-Analytics/src/main/java/io/pivotal/demo/ProbeCaptureRunner.java
@@ -1,7 +1,6 @@
 package io.pivotal.demo;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -19,27 +18,36 @@ public class ProbeCaptureRunner{ // implements CommandLineRunner {
 		
 				
 		logger.info("--------------------------------------");
-		Process tshark = Runtime.getRuntime().exec("sudo nohup tshark -i wlan1mon -I -f broadcast -R wlan.fc.subtype==4 -T fields -e frame.time_epoch -e wlan.sa -e radiotap.dbm_antsignal > probe_pipe &");
-		
-		if (!tshark.isAlive()){
-			
-			logger.severe("Process exited with code "+tshark.exitValue());	
-			logger.severe(new BufferedReader(new InputStreamReader(tshark.getInputStream())).readLine());
-			
-			BufferedReader errorStream = new BufferedReader(new InputStreamReader(tshark.getErrorStream()));
-			String errorLine = null;
-			while ((errorLine = errorStream.readLine())!=null){
-				logger.severe(errorLine);
-			}
-			
-		}
-		
-		tshark.waitFor();
-		
-		
-		InputStream in = new FileInputStream("probe_pipe");
+				
 		logger.info("Capturing tshark process output...");
+		InputStream in = new FileInputStream("probe_pipe");
 		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		
+		Thread t = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try{
+					Process tshark = Runtime.getRuntime().exec("sudo nohup tshark -i wlan1mon -I -f broadcast -R wlan.fc.subtype==4 -T fields -e frame.time_epoch -e wlan.sa -e radiotap.dbm_antsignal > probe_pipe &");				
+					if (!tshark.isAlive()){
+						
+						logger.severe("Process exited with code "+tshark.exitValue());	
+						logger.severe(new BufferedReader(new InputStreamReader(tshark.getInputStream())).readLine());
+						
+						BufferedReader errorStream = new BufferedReader(new InputStreamReader(tshark.getErrorStream()));
+						String errorLine = null;
+						while ((errorLine = errorStream.readLine())!=null){
+							logger.severe(errorLine);
+						}						
+					}									
+				}
+				catch(Exception e){
+					throw new RuntimeException(e);
+				}
+			}
+		});
+		t.start();
+		
 		String line = null;
 		while ( (line=br.readLine())!=null){
 			logger.fine(line);
