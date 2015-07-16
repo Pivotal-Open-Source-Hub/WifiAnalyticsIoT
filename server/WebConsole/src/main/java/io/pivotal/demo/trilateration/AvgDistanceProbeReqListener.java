5d451d328538bd40c6096804ff577fe10b9b8e9c
Frederico Melo <fmelo@Fredericos-MacBook-Air.local>
2015-07-15 23:01:48 -0400

    adding raspberrypi readme

---
+++ server/WebConsole/src/main/java/io/pivotal/demo/trilateration/AvgDistanceProbeReqListener.java
@@ -0,0 +1,57 @@
+package io.pivotal.demo.trilateration;
+
+import java.util.Properties;
+
+import io.pivotal.demo.ui.DeviceDistance;
+import io.pivotal.demo.ui.GeodeClient;
+
+import com.gemstone.gemfire.cache.Declarable;
+import com.gemstone.gemfire.cache.EntryEvent;
+import com.gemstone.gemfire.cache.util.CacheListenerAdapter;
+import com.gemstone.gemfire.pdx.PdxInstance;
+
+public class AvgDistanceProbeReqListener extends CacheListenerAdapter implements Declarable {
+
+	private static final int NUMBER_OF_AVG_PERIODS = 10;
+	
+	
+	@Override
+	public void afterCreate(EntryEvent event) {
+		super.afterCreate(event);
+		updateAverageDistance(event);
+	}
+
+	@Override
+	public void afterUpdate(EntryEvent event) {		
+		super.afterUpdate(event);
+		updateAverageDistance(event);
+	}
+
+	private void updateAverageDistance(EntryEvent e){
+		
+		Object obj = e.getNewValue();
+		String piId = null;
+		String deviceId = null;
+		if (obj instanceof PdxInstance){			
+			piId = (String)((PdxInstance)obj).getField("piId");
+			deviceId = (String)((PdxInstance)obj).getField("deviceId");
+		}
+		else throw new RuntimeException("new object is not PDX Instance.. it came as "+obj.getClass());
+		
+		double[] distances = GeodeClient.getInstance().getLatestDistanceMeasurements(piId, deviceId, NUMBER_OF_AVG_PERIODS);
+		
+		double sumOfDistances = 0;
+		for (int i=0; i<distances.length; i++){
+			sumOfDistances += distances[i];
+		}
+		
+		GeodeClient.getInstance().setDeviceDistance(new DeviceDistance(deviceId, piId, sumOfDistances / distances.length));
+	}
+
+	@Override
+	public void init(Properties arg0) {
+	}
+	
+	
+	
+}
